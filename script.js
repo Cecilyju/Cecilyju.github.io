e8147e5a6ece73de7ae6e41608974b3f9d01185d
Cecilyju <136142444+Cecilyju@users.noreply.github.com>
2025-05-24 01:51:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,185 @@
 const images = [
   {
-    name: "43634",
+    name: "冷艳少女",
     url: "https://api.18xo.eu.org/file/1747823385782_43634.png"
   },
   {
-    name: "43352",
+    name: "成熟美女",
     url: "https://api.18xo.eu.org/file/1747823210434_43352.png"
   },
   {
-    name: "浪漫花海",
-    url: "https://your-image-source.com/wallpaper3.jpg"
+    name: "八重神子",
+    url: "https://api.18xo.eu.org/file/1748010033600_【哲风壁纸】八重神子-原神-海滩.png"
   },
   {
-    name: "霓虹城市",
-    url: "https://your-image-source.com/wallpaper4.jpg"
+    name: "少女神子",
+    url: "https://api.18xo.eu.org/file/1748010034219_【哲风壁纸】少女-氛围-神子.png"
   },
   {
-    name: "清晨草原",
-    url: "https://your-image-source.com/wallpaper5.jpg"
+    name: "比基尼美女",
+    url: "https://api.18xo.eu.org/file/1748010037519_【哲风壁纸】性感美女-比基尼美女.png"
   },
   {
-    name: "极光天际",
-    url: "https://your-image-source.com/wallpaper6.jpg"
+    name: "性感少女",
+    url: "https://api.18xo.eu.org/file/1748010055601_【哲风壁纸】室内场景-性感造型.png"
+  },
+  {
+    name: "休闲少女",
+    url: "https://api.18xo.eu.org/file/1748010042044_【哲风壁纸】休闲姿态-室内场景.png"
+  },
+  {
+    name: "听歌女孩",
+    url: "https://api.18xo.eu.org/file/1748010054091_【哲风壁纸】听歌女孩-听歌的女孩.png"
+  },
+  {
+    name: "二次元美女",
+    url: "https://api.18xo.eu.org/file/1748010050594_【哲风壁纸】二次元美女-夜景人像.png"
+  },
+  {
+    name: "夜景少女",
+    url: "https://api.18xo.eu.org/file/1748010047267_【哲风壁纸】夜景背景-少女壁纸.png"
+  },
+  {
+    name: "休闲美女",
+    url: "https://api.18xo.eu.org/file/1748010050940_【哲风壁纸】休闲美女-圣诞树.png"
+  },
+  {
+    name: "室内少女",
+    url: "https://api.18xo.eu.org/file/1748010035613_【哲风壁纸】休闲-室内-少女.png"
+  },{
+    name: "动漫居家",
+    url: "https://api.18xo.eu.org/file/1748010051760_【哲风壁纸】动漫-动漫壁纸-居家.png"
+  },
+  {
+    name: "休憩少女",
+    url: "https://api.18xo.eu.org/file/1748010056578_【哲风壁纸】休憩-动漫-动漫壁纸.png"
+  },
+  {
+    name: "动漫少女",
+    url: "https://api.18xo.eu.org/file/1748010065590_【哲风壁纸】AI-AI少女.png"
+  },
+  {
+    name: "动漫夏日",
+    url: "https://api.18xo.eu.org/file/1748010036566_【哲风壁纸】动漫-动漫壁纸-夏日.png"
+  },
+  {
+    name: "人像美女",
+    url: "https://api.18xo.eu.org/file/1747824025883_43354.png"
+  },
+  {
+    name: "杨超越",
+    url: "https://api.18xo.eu.org/file/1747823856790_33256.png"
+  },
+  {
+    name: "鞠婧祎",
+    url: "https://api.18xo.eu.org/file/1747824026336_42083.png"
+  },
+  {
+    name: "冯宝宝",
+    url: "https://api.18xo.eu.org/file/1747823942632_34935.png"
+  },
+{
+    name: "动漫少女",
+    url: "https://api.18xo.eu.org/file/1747823922814_33881.png"
+  },
+{
+    name: "动漫少女",
+    url: "https://api.18xo.eu.org/file/1747824028829_36379.png"
+  },
+{
+    name: "古风美女",
+    url: "https://api.18xo.eu.org/file/1747824133424_44462.png"
+  },
+{
+    name: "Cosplay少女",
+    url: "https://api.18xo.eu.org/file/1747823758453_44997.png"
+  },
+{
+    name: "Cosplay少女",
+    url: "https://api.18xo.eu.org/file/1747823672938_44899.png"
+  },
+{
+    name: "Cosplay少女",
+    url: "https://api.18xo.eu.org/file/1747823522034_43840.png"
+  },
+{
+    name: "美少女",
+    url: "https://api.18xo.eu.org/file/1747823717414_44776.png"
+  },
+{
+    name: "古装美女",
+    url: "https://api.18xo.eu.org/file/1747823553014_43839.png"
+  },
+{
+    name: "居家美女",
+    url: "https://api.18xo.eu.org/file/1747823807919_45159.png"
+  },
+{
+    name: "美少女",
+    url: "https://api.18xo.eu.org/file/1747823074073_42245.png"
+  },
+{
+    name: "天台少女",
+    url: "https://api.18xo.eu.org/file/1747823054267_42626.png"
+  },
+{
+    name: "居家少女",
+    url: "https://api.18xo.eu.org/file/1747823329166_43447.png"
+  },
+{
+    name: "Cosplay 少女",
+    url: "https://api.18xo.eu.org/file/1747808247900_47770.png"
+  },
+{
+    name: "阳光 少女",
+    url: "https://api.18xo.eu.org/file/1747808150795_47710.png"
+  },
+{
+    name: "Cosplay 少女",
+    url: "https://api.18xo.eu.org/file/1747808236682_48177.png"
+  },
+  {
+    name: "镜子 美女",
+    url: "https://api.18xo.eu.org/file/1747808223400_45646.png"
+  },
+  {
+    name: "捆绑 少女",
+    url: "https://api.18xo.eu.org/file/1747808110238_45746.png"
+  },{
+    name: "Cosplay 少女",
+    url: "https://api.18xo.eu.org/file/1747808131652_45862.png"
+  },
+  {
+    name: "甜美 少女",
+    url: "https://api.18xo.eu.org/file/1747808148951_48482.png"
+  },
+  {
+    name: "古风 少女",
+    url: "https://api.18xo.eu.org/file/1747807976891_47792.png"
+  },
+  {
+    name: "网球 少女",
+    url: "https://api.18xo.eu.org/file/1747808009283_48772.png"
+  },
+  {
+    name: "朋克 少女",
+    url: "https://api.18xo.eu.org/file/1747807966399_47540.png"
+  },
+  {
+    name: "Cosplay 少女",
+    url: "https://api.18xo.eu.org/file/1747807809032_46845.png"
+  },
+  {
+    name: "居家 少女",
+    url: "https://api.18xo.eu.org/file/1747807867666_46213.png"
+  },
+  {
+    name: "短发 美少女",
+    url: "https://api.18xo.eu.org/file/1747807618152_46197.png"
+  },
+  {
+    name: "Cosplay 少女",
+    url: "https://api.18xo.eu.org/file/1747807676803_46844.png"
   }
 ];
 
