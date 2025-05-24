8537ef02f92a2d1c51a931961af061842586de97
Cecilyju <136142444+Cecilyju@users.noreply.github.com>
2025-05-24 13:32:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const wallpapers = [
   { name: "冷艳少女", url: "https://api.18xo.eu.org/file/1747823385782_43634.png" },
   { name: "海上 船 可爱女生长发", url: "https://api.18xo.eu.org/file/1748052035636_海上 船 可爱女生长发 4K壁纸全屏 3840x2400_彼岸图网.jpg" },
-  { name: "海边晨曦", url: "wallpapers/beach.jpg" },
-  { name: "城市夜景", url: "wallpapers/city.jpg" },
-  { name: "银河星空", url: "wallpapers/milkyway.jpg" }
+  { name: "赛博朋克风格 奇幻少女", url: "https://api.18xo.eu.org/file/1748052043512_赛博朋克风格奇幻少女 集原美电脑4k壁纸3840x2160_彼岸图网.jpg" },
+  { name: "报纸墙 可爱漂亮养眼美女", url: "https://api.18xo.eu.org/file/1748052034629_报纸墙 可爱漂亮养眼美女 4k壁纸 3840x2160_彼岸图网.jpg" },
+  { name: "漂亮少数民族美女", url: "https://api.18xo.eu.org/file/1748052162857_漂亮少数民族美女4k壁纸_彼岸图网.jpg" }
 ];
 
 const mainImage = document.getElementById("main-image");
