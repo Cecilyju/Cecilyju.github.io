879f7167b673ec3d2fcb8c36969ece6125f110f0
Cecilyju <136142444+Cecilyju@users.noreply.github.com>
2025-05-24 13:28:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 壁纸数据（可扩展为 JSON 或 API 加载）
 const wallpapers = [
   { name: "冷艳少女", url: "https://api.18xo.eu.org/file/1747823385782_43634.png" },
-  { name: "森林小径", url: "wallpapers/forest.jpg" },
+  { name: "海上 船 可爱女生长发", url: "https://api.18xo.eu.org/file/1748052035636_海上 船 可爱女生长发 4K壁纸全屏 3840x2400_彼岸图网.jpg" },
   { name: "海边晨曦", url: "wallpapers/beach.jpg" },
   { name: "城市夜景", url: "wallpapers/city.jpg" },
   { name: "银河星空", url: "wallpapers/milkyway.jpg" }
